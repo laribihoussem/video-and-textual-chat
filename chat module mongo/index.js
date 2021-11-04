f46bbc1f9b6145e3ef9fbc8eeaa349ee1d459150
laribi houssem <laribihoussem1998@gmail.com>
2021-11-04 15:46:09 +0100

    first commit

---
+++ chat module mongo/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Msg = require('./models/messages');
+const io = require('socket.io')(3000,  {
+    cors: {
+      methods: ["GET", "POST"]
+    }
+  })
+const mongoDB = 'mongodb://localhost:27017/messages';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('connected')
+}).catch(err => console.log(err))
+io.on('connection', (socket) => {
+    Msg.find().then(result => {
+        socket.emit('output-messages', result)
+    })
+    console.log('a user connected');
+    socket.emit('message', 'Hello world');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chatmessage', msg => {
+        const message = new Msg({ msg });
+        message.save().then(() => {
+            io.emit('message', msg)
+        })
+
+    })
+});
